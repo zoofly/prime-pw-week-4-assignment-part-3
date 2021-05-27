3c3c5e915044c2f3bc48e436519dae4c917593eb
Kashia Moua <kserenitym@gmail.com>
2021-05-27 10:30:49 -0500

    just started

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,16 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket=[" "];
+
+function addItem(value){
+
+  return true;
+}
+
+//end addItem
+
+function listItems(){
+  for(let i=0; i<=basket.length;i++);
+  console.log()
+}
